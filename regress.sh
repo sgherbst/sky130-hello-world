0acd596eda2c3ae24bbe06036d4352864f00ad85
Steven Herbst <sherbst@stanford.edu>
2020-12-15 11:52:07 -0800

    first test

--- regress.sh
+++ regress.sh
@@ -1,13 +1,20 @@
 # install magic
 git clone https://github.com/RTimothyEdwards/magic.git
+cd magic
 git checkout magic-8.3
 ./configure
 make
 sudo make install
+cd ..
 
 # install netgen
 git clone https://github.com/RTimothyEdwards/netgen.git
+cd netgen
 git checkout netgen-1.5
 ./configure
 make
 sudo make install
+cd ..
+
+# install skywater-pdk
+
